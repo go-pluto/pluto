8f4713dc488eaa76f8b0c5b596b2d41425de51b7
Lennart Oldenburg <hello@lennartoldenburg.de>
2016-11-21 16:59:59 +0100

    Improved package documentations to keep goreportcard happy.

--- comm/doc.go
+++ comm/doc.go
@@ -1,4 +1,5 @@
 /*
-Package comm
+Package comm implements network communication that is reliable and causally-ordered to
+a group of nodes and respective receiving parts.
 */
 package comm
--- crdt/doc.go
+++ crdt/doc.go
@@ -1,4 +1,5 @@
 /*
-Package crdt
+Package crdt implements the necessary abstraction of a Maildir file system into
+a CRDT representation usable to sync across multiple replicas.
 */
 package crdt
