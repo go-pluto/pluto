277bc1cd9fb688471774af4012377e4e02ef32d6
Matthias Loibl <mail@matthiasloibl.com>
2017-05-04 17:17:02 +0200

    Move the PlainAuthenticator interface to the only consumer

--- imap/distributor.go
+++ imap/distributor.go
@@ -14,13 +14,32 @@ import (
 	"github.com/numbleroot/pluto/crypto"
 )
 
+// Interfaces
+
+// PlainAuthenticator defines the methods required to
+// perform an IMAP AUTH=PLAIN authentication in order
+// to reach authenticated state (also LOGIN).
+type PlainAuthenticator interface {
+
+	// GetWorkerForUser allows us to route an IMAP request to the
+	// worker node responsible for a specific user.
+	GetWorkerForUser(workers map[string]config.Worker, id int) (string, error)
+
+	// AuthenticatePlain will be implemented by each of the
+	// authentication methods of type PLAIN to perform the
+	// actual part of checking supplied credentials.
+	AuthenticatePlain(username string, password string, clientAddr string) (int, string, error)
+}
+
+// Structs
+
 // Distributor struct bundles information needed in
 // operation of a distributor node.
 type Distributor struct {
 	lock          *sync.RWMutex
 	Socket        net.Listener
 	IntlTLSConfig *tls.Config
-	AuthAdapter   auth.PlainAuthenticator
+	AuthAdapter   PlainAuthenticator
 	Connections   map[string]*tls.Conn
 	Config        *config.Config
 }
