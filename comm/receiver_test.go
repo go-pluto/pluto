43f344b2112a3e4751a087810a7d61432c67683c
Lennart Oldenburg <hello@lennartoldenburg.de>
2017-06-21 12:37:02 +0200

    Extended Incoming() test to check more usefully for multiple values written to shared log.

--- comm/receiver_test.go
+++ comm/receiver_test.go
@@ -1,6 +1,9 @@
 package comm
 
 import (
+	"bytes"
+	"fmt"
+	"math"
 	"os"
 	"sync"
 	"testing"
@@ -13,6 +16,26 @@ import (
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
 )
+
+// Variables
+
+var (
+	confStatus = uint32(0)
+
+	write1 = []byte("hello")
+	write2 = []byte("\nwhat\r\tabout!\"§$%&/()=strange#+?`?`?°°°characters")
+	write3 = []byte("∰☕✔😉")
+	write4 = []byte("1234567890")
+	write5 = []byte(fmt.Sprintf("%g", math.MaxFloat64))
+
+	check1 = []byte("5;hello")
+	check2 = []byte("53;\nwhat\r\tabout!\"§$%&/()=strange#+?`?`?°°°characters")
+	check3 = []byte("13;∰☕✔😉")
+	check4 = []byte("10;1234567890")
+	check5 = []byte(fmt.Sprintf("23;%g", math.MaxFloat64))
+)
+
+// Functions
 
 // TestTriggerMsgApplier executes a white-box unit
 // test on implemented TriggerMsgApplier() function.
@@ -88,9 +111,10 @@ func TestIncoming(t *testing.T) {
 	_, err = recv.updLog.Seek(0, os.SEEK_SET)
 	assert.Nilf(t, err, "expected resetting of position in update log not to fail but received: %v", err)
 
-	// Execute function to test.
+	// Value 1.
+	// Write first value to log file.
 	conf, err := recv.Incoming(context.Background(), &BinMsg{
-		Data: []byte("hallo"),
+		Data: write1,
 	})
 	assert.Nilf(t, err, "expected nil error for Incoming() but received: %v", err)
 
@@ -98,12 +122,90 @@ func TestIncoming(t *testing.T) {
 	<-recv.msgInLog
 
 	// Validate received confirmation struct.
-	assert.Equalf(t, uint32(0), conf.Status, "expected conf to carry Status=0 but found: %v", conf.Status)
+	assert.Equalf(t, confStatus, conf.Status, "expected conf to carry Status=0 but found: %v", conf.Status)
 
 	// Read content of log file for inspection.
 	content, err := ioutil.ReadFile(tmpLogFile)
 	assert.Nilf(t, err, "expected nil error for ReadFile() but received: %v", err)
 
 	// Check for correct content.
-	assert.Equalf(t, []byte("5;hallo"), content, "expected '5;hallo' in log file but found: %v", content)
+	assert.Equalf(t, check1, content, "expected '%s' in log file but found: %v", check1, content)
+
+	// Value 2.
+	// Write second value to file.
+	conf, err = recv.Incoming(context.Background(), &BinMsg{
+		Data: write2,
+	})
+	assert.Nilf(t, err, "expected nil error for Incoming() but received: %v", err)
+
+	<-recv.msgInLog
+
+	assert.Equalf(t, confStatus, conf.Status, "expected conf to carry Status=0 but found: %v", conf.Status)
+
+	content, err = ioutil.ReadFile(tmpLogFile)
+	assert.Nilf(t, err, "expected nil error for ReadFile() but received: %v", err)
+
+	content = bytes.TrimPrefix(content, check1)
+
+	assert.Equalf(t, check2, content, "expected '%s' in log file but found: %v", check2, content)
+
+	// Value 3.
+	// Write third value to file.
+	conf, err = recv.Incoming(context.Background(), &BinMsg{
+		Data: write3,
+	})
+	assert.Nilf(t, err, "expected nil error for Incoming() but received: %v", err)
+
+	<-recv.msgInLog
+
+	assert.Equalf(t, confStatus, conf.Status, "expected conf to carry Status=0 but found: %v", conf.Status)
+
+	content, err = ioutil.ReadFile(tmpLogFile)
+	assert.Nilf(t, err, "expected nil error for ReadFile() but received: %v", err)
+
+	content = bytes.TrimPrefix(content, check1)
+	content = bytes.TrimPrefix(content, check2)
+
+	assert.Equalf(t, check3, content, "expected '%s' in log file but found: %v", check3, content)
+
+	// Value 4.
+	// Write fourth value to file.
+	conf, err = recv.Incoming(context.Background(), &BinMsg{
+		Data: write4,
+	})
+	assert.Nilf(t, err, "expected nil error for Incoming() but received: %v", err)
+
+	<-recv.msgInLog
+
+	assert.Equalf(t, confStatus, conf.Status, "expected conf to carry Status=0 but found: %v", conf.Status)
+
+	content, err = ioutil.ReadFile(tmpLogFile)
+	assert.Nilf(t, err, "expected nil error for ReadFile() but received: %v", err)
+
+	content = bytes.TrimPrefix(content, check1)
+	content = bytes.TrimPrefix(content, check2)
+	content = bytes.TrimPrefix(content, check3)
+
+	assert.Equalf(t, check4, content, "expected '%s' in log file but found: %v", check4, content)
+
+	// Value 5.
+	// Write fifth value to file.
+	conf, err = recv.Incoming(context.Background(), &BinMsg{
+		Data: write5,
+	})
+	assert.Nilf(t, err, "expected nil error for Incoming() but received: %v", err)
+
+	<-recv.msgInLog
+
+	assert.Equalf(t, confStatus, conf.Status, "expected conf to carry Status=0 but found: %v", conf.Status)
+
+	content, err = ioutil.ReadFile(tmpLogFile)
+	assert.Nilf(t, err, "expected nil error for ReadFile() but received: %v", err)
+
+	content = bytes.TrimPrefix(content, check1)
+	content = bytes.TrimPrefix(content, check2)
+	content = bytes.TrimPrefix(content, check3)
+	content = bytes.TrimPrefix(content, check4)
+
+	assert.Equalf(t, check5, content, "expected '%s' in log file but found: %v", check5, content)
 }
