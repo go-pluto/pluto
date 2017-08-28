23bde119878d50c73b467b941968d517eb937d80
Lennart Oldenburg <hello@lennartoldenburg.de>
2017-08-28 15:00:32 +0200

    Enabled performance profiling with pprof.

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ import (
 
 	"crypto/tls"
 	"io/ioutil"
+	"net/http"
 	"path/filepath"
+
+	_ "net/http/pprof"
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
@@ -166,6 +169,14 @@ func main() {
 	}
 
 	plutoMetrics := NewPlutoMetrics(conf.Distributor.PrometheusAddr)
+
+	go func() {
+
+		// Enable performance profiling with pprof.
+		level.Info(logger).Log(
+			"msg", http.ListenAndServe("127.0.0.1:6060", nil),
+		)
+	}()
 
 	// Initialize and run a node of the pluto
 	// system based on passed command line flag.
